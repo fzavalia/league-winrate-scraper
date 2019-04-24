489878fdbb22066269a0fd6de215273f34d0aaa8
Fernando Zavalia <fernando.zavalia@letflow.com.ar>
2019-04-24 12:57:54 -0300

    Refactor

--- index.js
+++ index.js
@@ -7,43 +7,9 @@ const champs = require('./champs.json')
 
 const baseUrl = 'https://www.leagueofgraphs.com/es/rankings/summoners/'
 
-const logCurrentScrapedChamp = (champ, res) => {
-    console.log('Scraping ' + champ)
-    return res
-}
-
-const getWinPercentage = res =>
-    Promise.resolve(cheerio.load(res))
-        .then($ => $('.data_table > tbody').children().map((_, e) => $(e).children('td:nth-child(4)').text()).toArray())
-
-const scrapChamp = champ =>
-    axios.get(baseUrl + champ)
-        .then(res => res.data)
-        .then(res => logCurrentScrapedChamp(champ, res))
-        .then(res => getWinPercentage(res))
-        .then(res => res.map(x => x.toString()))
-        .then(res => res.map(x => x.match(/\d.?\.\d/)))
-        .then(res => res.filter(x => x != null))
-        .then(res => res.map(x => x[0]))
-        .then(res => res.map(parseFloat))
-        .then(res => res.reduce((acc, next) => acc + next, 0) / res.length)
-        .then(res => ([champ, res.toFixed(2)]))
-
-const requests = champs.map(scrapChamp)
-
 const resultsFolderPath = path.resolve(__dirname, 'results')
 
-const createResultsFolder = () => {
-    if (!fs.existsSync(resultsFolderPath)) {
-        fs.mkdirSync(resultsFolderPath)
-    }
-}
-
-const storeResults = res =>
-    fs.writeFileSync(
-        path.resolve(resultsFolderPath, datefns.format(new Date(), 'YYYY-MM-DD') + '.csv'),
-        res.map(pair => pair.join(',')).join('\n')
-    )
+const requests = champs.map(champ => axios.get(baseUrl + champ).then(res => handleChampionPageResponse(res, champ)))
 
 console.log('Scraping... \n')
 
@@ -54,3 +20,50 @@ Promise.all(requests)
     })
     .then(() => console.log('\nScraping complete!'))
     .catch(console.error)
+
+function handleChampionPageResponse(res, champ) {
+
+    console.log('Scraping ' + champ)
+
+    const percentageContainers = getPercentageContainers(res.data)
+    const percentages = getPercentages(percentageContainers)
+    const total = percentages.reduce((acc, next) => acc + next, 0)
+    const mean = (total / percentages.length).toFixed(2)
+
+    return [champ, mean]
+}
+
+function getPercentageContainers(html) {
+
+    const $ = cheerio.load(html)
+
+    return $('.data_table > tbody')
+        .children()
+        .map((_, element) => $(element).children('td:nth-child(4)').text())
+        .toArray()
+}
+
+function getPercentages(percentageContainers) {
+    return percentageContainers
+        .map(element => element.toString())
+        .map(elementAsString => elementAsString.match(/\d.?\.\d/))
+        .filter(percentageMatch => percentageMatch != null)
+        .map(percentageMatch => percentageMatch[0])
+        .map(percentageAsString => parseFloat(percentageAsString))
+}
+
+function createResultsFolder() {
+    if (!fs.existsSync(resultsFolderPath)) {
+        fs.mkdirSync(resultsFolderPath)
+    }
+}
+
+function storeResults(res) {
+
+    const resultFileName = path.resolve(resultsFolderPath, datefns.format(new Date(), 'YYYY-MM-DD') + '.csv')
+    const resultAsCsv = res.map(pair => pair.join(',')).join('\n')
+
+    fs.writeFileSync(resultFileName, resultAsCsv)
+
+    console.log('\nResults stored in ' + resultFileName)
+}
