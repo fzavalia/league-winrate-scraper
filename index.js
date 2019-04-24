e789944c5ee7b230387b2d888a708c5276f17d57
Fernando Zavalia <fernando@pop-os.localdomain>
2019-04-24 02:55:54 -0300

    Change writefile format

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const axios = require('axios').default
 const cheerio = require('cheerio')
 const fs = require('fs')
 const path = require('path')
-const champs = require('./champs.json').slice(0, 5)
+const champs = require('./champs.json')
+const datefns = require('date-fns')
 
 const baseUrl = 'https://www.leagueofgraphs.com/es/rankings/summoners/'
 
@@ -20,17 +21,17 @@ const makeRequest = champ =>
         .then(res => res.map(x => x[0]))
         .then(res => res.map(parseFloat))
         .then(res => res.reduce((acc, next) => acc + next, 0) / res.length)
-        .then(res => ({ champ, mean: res }))
+        .then(res => ({ champ, value: res }))
 
 const requests = champs.map(makeRequest)
 
 const writeResultToFile = res => {
-    const writePath = path.resolve(__dirname, 'data')
+    const writePath = path.resolve(__dirname, 'results')
     if (!fs.existsSync(writePath)) {
         fs.mkdirSync(writePath)
     }
     fs.writeFileSync(
-        path.resolve(writePath, new Date().toDateString()),
+        path.resolve(writePath, datefns.format(new Date(), 'YYYY-MM-DD') + '.json'),
         JSON.stringify(res, null, 2)
     )
 }
